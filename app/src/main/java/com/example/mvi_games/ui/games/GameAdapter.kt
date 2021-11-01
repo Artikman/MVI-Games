312f7846bef096e1a973033303eb8f9b3471b6f9
Артём <47893341+Artikman@users.noreply.github.com>
2021-11-01 15:31:46 +0300

    Update GameAdapter.kt

--- app/src/main/java/com/example/mvi_games/ui/games/GameAdapter.kt
+++ app/src/main/java/com/example/mvi_games/ui/games/GameAdapter.kt
@@ -1,40 +1,51 @@
 package com.example.mvi_games.ui.games
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
+import com.example.domain.common.DateUtil
 import com.example.mvi_games.databinding.RowGameItemLayoutBinding
 import com.example.mvi_games.model.GameUiModel
+import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
 
-class GameAdapter constructor(
-    private val context: Context
-) : ListAdapter<GameUiModel, GameViewHolder>(GameItemDiffUtil()) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
-        val binding = RowGameItemLayoutBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return GameViewHolder(binding = binding, context = context)
-    }
-
-    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
-        val games = getItem(position)
-        if (games != null) {
-            holder.bind(games)
+fun gameItemAdapterDelegate() =
+    adapterDelegateViewBinding<GameUiModel, GameUiModel, RowGameItemLayoutBinding>(
+        { layoutInflater, parent ->
+            RowGameItemLayoutBinding.inflate(
+                layoutInflater,
+                parent,
+                false
+            )
+        }
+    ) {
+        bind {
+            with(binding) {
+                Glide.with(context)
+                    .load(item.thumbnail)
+                    .into(imageViewGames)
+                textViewTitle.text = item.title
+                textViewDescription.text = item.shortDescription
+                textViewTime.text = DateUtil.getTimeAgo(item.releaseDate)
+            }
         }
     }
-}
 
-class GameItemDiffUtil : DiffUtil.ItemCallback<GameUiModel>() {
-    override fun areItemsTheSame(oldItem: GameUiModel, newItem: GameUiModel): Boolean {
-        return oldItem.id == newItem.id
+internal val gameItemDiffCallback = itemCallback<GameUiModel>(
+    areItemsTheSame = { oldItem, newItem ->
+        oldItem.id == newItem.id
+    },
+    areContentsTheSame = { oldItem, newItem ->
+        oldItem == newItem
+    },
+    getChangePayload = { _, newItem ->
+        newItem
     }
+)
 
-    override fun areContentsTheSame(oldItem: GameUiModel, newItem: GameUiModel): Boolean {
-        return oldItem == newItem
+
+class GameAdapter : AsyncListDifferDelegationAdapter<GameUiModel>(
+    gameItemDiffCallback
+) {
+    init {
+        delegatesManager.addDelegate(gameItemAdapterDelegate())
     }
 }
